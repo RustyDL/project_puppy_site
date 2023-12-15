36c1634504c309c3d106e0fb2dd5408baab328f1
rustydl <jason.m.garcia04@gmail.com>
2023-12-14 20:58:14 -0500

    test trail

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,37 @@
-import {puppyList} from './data.js'
-import { useState } from 'react'
+import React, { useState } from "react";
+import { puppyList } from "./data.js";
+import "./App.css";
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  
-  console.log(puppyList)
+  // Initialize the puppies state with the puppyList array
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  console.log(featuredPup);
 
   return (
-    
-    <>
-      <div> 
-                { 
-          puppies.map((puppy) => {
-            return <p>{puppy.name}</p>
-          })
-       }
-
+    <div className="root">
+      <div className="card">
+        {puppies.map((puppy) => {
+          return (
+            <p key={puppy.id} onClick={() => setFeatPupId(puppy.id)}>
+              {puppy.name}
+            </p>
+          );
+        })}
       </div>
-    </>
-  )
+      {featPupId && featuredPup && (
+        <div className="card">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>{" "}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
